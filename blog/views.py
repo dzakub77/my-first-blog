c83548b6b59f1862d32064c792f28ba6f97a6686
dzakub77 <jak.inglot@gmail.com>
2020-01-16 12:27:16 +0100

    Modyfikowanie postów

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from .models import Post  # kropka oznacza biezacy katalog lub aplikacja znajduje sie w tym samym katalogu
+from django.utils import timezone
+
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #zmienna dla QuerySeru.
+    return render(request, 'blog/post_list.html', {'posts': posts})
