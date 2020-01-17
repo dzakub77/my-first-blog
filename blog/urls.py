637df15549a125f087df8a77163c61442345fb94
dzakub77 <jak.inglot@gmail.com>
2020-01-17 10:18:20 +0100

    add post detail

--- blog/urls.py
+++ blog/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     path('', views.post_list, name='post_list'), #nazwa URL
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
 ]
+# 'post/<int:pk>/' - ogólnie jest to wzorzec URL: post/ - oznacza że adres URL powinien
+# zaczynać się od słowa post, po którym nastąpi /. <int:pk> - oznacza że Django
+# spodziewa się liczby całkowitej i przekaże jej wartość do widoku jako zmienną pk
+# i znów potrzebujemy / zanim zakonczymy wzorzec URL
--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from .models import Post  # kropka oznacza biezacy katalog lub aplikacja znajduje sie w tym samym katalogu
 from django.utils import timezone
+from django.shortcuts import render, get_object_or_404
 
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     #zmienna dla QuerySeru.
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
